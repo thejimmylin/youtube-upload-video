02fb841dac9a836e75b59e65c07bb0309265be57
Jimmy Lin <b00502013@gmail.com>
2023-04-09 04:40:46 +0800

    change the dir structure

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
 # Youtube Upload video
 
-Before using, get a `client_secrets.json` from your GCP dashboard and put it in root.
+Upload Youtube videos with JS easily.
+
+- `src/index.js` is the source code.
+- `src/test.js` is the test.
 
 # Usage
 
-Upload youtube upload video
+Before using, get a `client_secrets.json` from your GCP dashboard and put it in `data`.
+Run the test
 
 ```
-python main.py
+npm run test
 ```
-
-Make dummy video
-
-```
-python make_dummy_video.py
-```
-
-# Debug
-
-To debug, follow the console error messages and ask ChatGPT 4.
---
+++ src/index.js
@@ -0,0 +1,82 @@
+import fs from "fs";
+import { OAuth2Client } from "google-auth-library";
+import { google } from "googleapis";
+import http from "http";
+import open from "open";
+import { URL } from "url";
+
+const SCOPE = ["https://www.googleapis.com/auth/youtube"];
+const CLIENT_SECRET_FILE = "data/client_secret.json";
+const TOKEN_FILE = "data/token.json";
+
+function enableDestroy(server) {
+  const connections = new Map();
+
+  server.on("connection", (conn) => {
+    const key = `${conn.remoteAddress}:${conn.remotePort}`;
+    connections.set(key, conn);
+    conn.on("close", () => {
+      connections.delete(key);
+    });
+  });
+
+  server.destroy = (cb) => {
+    server.close(cb);
+    for (const conn of connections.values()) {
+      conn.destroy();
+    }
+  };
+}
+
+async function authenticate(clientSecretFile, token_file, scope) {
+  const content = fs.readFileSync(clientSecretFile, "utf8");
+  const keyFile = JSON.parse(content);
+  const keys = keyFile.installed || keyFile.web;
+
+  const redirectUri = new URL(keys.redirect_uris[0]);
+  const client = new OAuth2Client({
+    clientId: keys.client_id,
+    clientSecret: keys.client_secret,
+  });
+
+  if (fs.existsSync(token_file)) {
+    const token = fs.readFileSync(token_file, "utf8");
+    const credentials = JSON.parse(token);
+    client.setCredentials(credentials);
+    return client;
+  } else {
+    return new Promise((resolve, reject) => {
+      const server = http.createServer(async (req, res) => {
+        try {
+          const url = new URL(req.url, "http://localhost:3000");
+          const searchParams = url.searchParams;
+          const code = searchParams.get("code");
+          const { tokens } = await client.getToken({ code: code, redirect_uri: redirectUri.toString() });
+          client.setCredentials(tokens);
+          fs.writeFileSync(token_file, JSON.stringify(tokens));
+          resolve(client);
+          res.end("Authentication successful! Please return to the console.");
+        } catch (e) {
+          reject(e);
+        } finally {
+          server.destroy();
+        }
+      });
+
+      server.listen(Number(redirectUri.port), () => {
+        const authorizeUrl = client.generateAuthUrl({
+          redirect_uri: redirectUri.toString(),
+          access_type: "offline",
+          scope,
+        });
+        open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
+      });
+      enableDestroy(server);
+    });
+  }
+}
+
+export async function getYoutube(clientSecretFile = CLIENT_SECRET_FILE, token_file = TOKEN_FILE) {
+  const oAuth2Client = await authenticate(clientSecretFile, token_file, SCOPE);
+  return google.youtube({ version: "v3", auth: oAuth2Client });
+}
